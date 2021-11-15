56c4516919f4fc806cc9ab729afc107ed2f4c575
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-15 11:14:33 +0700

    initial commit

---
+++ bin/dartapp301.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Variable Declaratin and Variable Initialization
+  String msg = "Welcome to Dart"; //Local Variable
+  String fullname = "Mark Sakaberg";
+  int age = 25;
+  int lastBirthDay = 2020;
+  int currentBirthDay = 2021;
+  int current = currentBirthDay - lastBirthDay;
+
+
+  //print(msg + fullname); //String Concatenation
+  //print(age);
+  //print('${currentBirthDay - lastBirthDay}'); //String Interpolation ${expression}.
+  print('$msg, My name is $fullname, My age is $age, $current'); //A string literal.
+
+}
